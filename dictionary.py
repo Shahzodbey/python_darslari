e0ab16933bf82db97c6819462942183e7f5aaca6
Shahzodbey <91586451+Shahzodbey@users.noreply.github.com>
2021-10-04 08:06:00 +0500

    yangi vazifalar

---
+++ dictionary.py
@@ -0,0 +1,17 @@
+otam = {'ismi':'mavlutdin', 'tyil':1954,'viloyat':'samarqand'}
+tyil = otam['tyil']
+vil = otam['viloyat']
+print(f"Otamning ismi {otam['ismi'].title()}, {tyil}-yilda, {vil.title()} viloyatida tug'ilgan")
+
+python_izohli_lugati = {
+    'integer':"Butun son",
+    'float':"O'nlik son",
+    'string':"Matn",
+    'list':"Ro'yxat",
+    'tuple':"O'zgarmas ro'yxat"}
+kalit = input("Kalit so'z kiriting:").lower()
+tarjima = python_izohli_lugati.get(kalit)
+if tarjima==None:
+    print("Bunday so'z mavjud emas")
+else:
+    print(f"{kalit.title()} so'zi {tarjima} deb tarjima qilinadi")
